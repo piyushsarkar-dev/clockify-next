a2aeef01c37964130a2c82a63eec148e239dd80c
Mr Piyush <hi.mrpiyush@gmail.com>
2026-02-20 19:29:39 +0530

    Update Clock.tsx

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -7,9 +7,12 @@ const Clock = () => {
 	const [hour, setHours] = useState("00");
 	const [minutes, setMinutes] = useState("00");
 	const [second, setSecoend] = useState("00");
-	const [ampm, setAmpm] = useState(format(new Date(), "eeee,dd LLLL yyyy"));
+	const [ampm, setAmpm] = useState(format(new Date(), "hh"));
+	const [date, setDate] = useState(format(new Date(), "eeee, dd LLLL yyyy"));
 
-	useEffect(() => {}, []);
+	useEffect(() => {
+		const interval = setInterval(() => {}, 1000);
+	}, []);
 
 	return (
 		<section>
@@ -25,7 +28,7 @@ const Clock = () => {
 						<span className="inline-block h-4 w-4 cursor-pointer rounded-full bg-green-500"></span>
 					</div>
 				</div>
-				<div className="flex h-full flex-col items-center justify-center gap-3 font-mono text-7xl">
+				<div className="flex h-full flex-col items-center justify-center gap-3 font-mono text-4xl">
 					<div className="flex items-center justify-center">
 						<div>{hour}</div>
 						<span>:</span>
@@ -35,7 +38,7 @@ const Clock = () => {
 
 						<div>{ampm}</div>
 					</div>
-					<div className="">Date</div>
+					<div className="">{date}</div>
 				</div>
 			</div>
 		</section>
